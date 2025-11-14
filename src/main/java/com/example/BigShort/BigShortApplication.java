92f15f889991aae1a64fad12435b91cc40bc13ee
esraakhalifa <esraakhalifa5@gmail.com>
2025-11-14 20:39:57 +0200

    Initial commit

---
+++ src/main/java/com/example/BigShort/BigShortApplication.java
@@ -0,0 +1,13 @@
+package com.example.BigShort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigShortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigShortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BigShort/BigShortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BigShort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigShortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
